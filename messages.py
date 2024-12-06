b0088ecc7ae4bec2b6b38e2791a60f4f562e67dc
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-12-05 23:21:44 -0300

    feat: substituindo emojis unicode para álias dos emotes do discord

--- messages.py
+++ messages.py
@@ -1,13 +1,13 @@
 messages = [
-    "Psst... Novo vídeo no ar! Corre lá antes que todo mundo veja primeiro! 🚀",
-    "Alerta de conteúdo fresquinho! Acabou de sair um vídeo novinho pra você curtir. 😉",
-    "Tá de boa? Então vem de play no novo vídeo e se liga no que o {username} preparou hoje! ✨",
-    "Última chamada para o hype train! Novo vídeo liberado! Bora? 🎬",
+    "Psst... Novo vídeo no ar! Corre lá antes que todo mundo veja primeiro! :rocket:",
+    "Alerta de conteúdo fresquinho! Acabou de sair um vídeo novinho pra você curtir. :wink:",
+    "Tá de boa? Então vem de play no novo vídeo e se liga no que o {username} preparou hoje! :stars:",
+    "Última chamada para o hype train! Novo vídeo liberado! Bora? :tickets:",
     "Tem vídeo novo pra você comentar assim: \"Apaga o canal, {username}\"",
     "Ouviu isso? É o som do lacre! Confira o novo vídeo do lacrezin"
-    "Spoiler: tá incrível (eu acho)! Clica aí e confere o vídeo novo que acabou de chegar. 🚀",
-    "Vibes certas, conteúdo certo! Não perde o vídeo novo que já tá esperando por você! 🔥",
+    "Spoiler: tá incrível (eu acho)! Clica aí e confere o vídeo novo que acabou de chegar. :rocket:",
+    "Vibes certas, conteúdo certo! Não perde o vídeo novo que já tá esperando por você! :fire:",
     "Alimente sua curiosidade! Novo vídeo subiu e tá imperdível. Bora aprender e se divertir?",
-    "Aviso importante: Se você ignorar, vai perder o melhor vídeo do dia. 🌟",
+    "Aviso importante: Se você ignorar, vai perder o melhor vídeo do dia. :star2:",
     "👾 Game over de ficar parado: dá play no vídeo novo agora e bora nessa jornada!"
 ]
